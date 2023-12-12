4ff03678cc5b1e4bb3ec31efe9572f4f035ac682
JinWoo Shin <sto134679@gmail.com>
2023-12-12 22:29:47 +0900

    코테 29번 해결

---
+++ CodingTest/Test029.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+        int Stone = stones.size();
+        int Jewel = 0;
+
+        // 첫 번째 문자열의 각 알파벳을 두 번째 문자열에서 찾아서 제거
+        for (char c : jewels)
+        {
+            size_t found = stones.find(c);
+
+            // 해당 알파벳을 찾았을 경우 제거
+            while (found != string::npos)
+            {
+                stones.erase(found, 1);
+
+                found = stones.find(c);
+            }
+
+            Jewel = stones.size();
+            
+        }
+
+        int Res = Stone - Jewel;
+
+        return Res;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    string str1 = "aA";
+    string str2 = "aAABBBB";
+
+    sol.numJewelsInStones(str1, str2);
+
+    return 0;
+}
