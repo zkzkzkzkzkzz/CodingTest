97a87afa048ff2697d8542566d5508c23ad1e7e7
JinWoo Shin <sto134679@gmail.com>
2024-01-03 21:30:47 +0900

    코테 81번 해결

--- CodingTest/Test081-134.cpp
+++ CodingTest/Test081-134.cpp
@@ -12,69 +12,39 @@ public:
         // 주유소는 원형으로 위치하기 때문에 해당 인덱스에서 다음 인덱스로 이동해야함
         // 마지막 인덱스일 경우 처음 인덱스로 이동한다
         // fuel = gas - cost라 할 때, vector<int> fuel의 원소는 다음과 같다
-        //   0   1  2   3  4
-        // {-2, -2, -2, 3, 3}
+        //   0  1   2   3   4
+        // { 1, -3, 1, -2,  3}
         // 모든 원소를 더했을 때 0이상이므로 모든 주유소를 돌 수 있다는 걸 일단 알 수 있다
         // 시작 인덱스는 3 또는 4인데, 해당 인덱스부터 차례대로 더해가다 중간에 음수가 나온다면
         // 해당 인덱스는 시작점이 아니다 -> 시간 초과
 
-
-        vector<int> fuel(gas.size());
-        vector<int> idx;
-        int Res = -1;
-        int Valid = 0;
-        int Sum = 0;
+        int fuel = 0;
+        int StartIdx = 0;
+        int totalGas = 0;
+        int totalCost = 0;
 
         for (int i = 0; i < gas.size(); ++i)
         {
-            fuel[i] = gas[i] - cost[i];
-            Valid += fuel[i];
+            totalGas += gas[i];
+            totalCost += cost[i];
 
-            if (fuel[i] >= 0)
+            fuel += gas[i] - cost[i];
+
+            // fuel이 0 미만이라는 건 해당 시작지점에선 모든 주유소를 돌 수 없다는 것을 의미
+            if (fuel < 0)
             {
-                idx.push_back(i);
+                // 다음 주유소부터 다시 순회
+                StartIdx = i + 1;
+                fuel = 0;
             }
         }
 
-        if (Valid < 0)
+        if (totalGas < totalCost)
         {
             return -1;
         }
-
-        for (int i = 0; i < idx.size(); ++i)
-        {
-            int temp = idx[i];
-
-            for (int j = temp; j < fuel.size(); ++j)
-            {
-                if (Sum < 0)
-                {
-                    break;
-                }
-
-
-                Sum += fuel[j];
-            }
-
-            for (int j = 0; j < temp; ++j)
-            {
-                if (Sum < 0)
-                {
-                    break;
-                }
-
-                Sum += fuel[j];
-            }
-
-            if (Sum >= 0)
-            {
-                Res = idx[i];
-            }
-            
-            Sum = 0;
-        }
-
-        return Res;
+        
+        return StartIdx;
     }
 };
 
